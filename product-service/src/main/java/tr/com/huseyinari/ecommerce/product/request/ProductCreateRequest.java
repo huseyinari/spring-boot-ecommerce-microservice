12f5c48519e66b643502ae11afec62309a5b7fe1
Huseyin ARI <hsynari1060@gmail.com>
2025-04-13 13:07:26 +0300

    create product validation

--- product-service/src/main/java/tr/com/huseyinari/ecommerce/product/request/ProductCreateRequest.java
+++ product-service/src/main/java/tr/com/huseyinari/ecommerce/product/request/ProductCreateRequest.java
@@ -1,11 +1,17 @@
 package tr.com.huseyinari.ecommerce.product.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 
 
 public record ProductCreateRequest(
+    @NotBlank(message = "Lütfen ürün ismi giriniz.")
     String name,
     String description,
+    @NotNull(message = "Lütfen ürün fiyatı giriniz.")
     BigDecimal price,
+    @NotNull(message = "Lütfen kategori seçiniz.")
     Long categoryId
 ) {}
