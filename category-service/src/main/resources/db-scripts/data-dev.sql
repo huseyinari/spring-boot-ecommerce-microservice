ae4331fbe144e1949c2669d159fcd3c9aa5ea2bc
Huseyin ARI <hsynari1060@gmail.com>
2025-06-29 00:08:47 +0300

    category ornek data eklendi

--- category-service/src/main/resources/db-scripts/data-dev.sql
+++ category-service/src/main/resources/db-scripts/data-dev.sql
@@ -1,27 +1,53 @@
 INSERT INTO public.category
 (id, category_name, image_url, total_product_count, parent_id, created_by, created_date, updated_by, updated_date)
-VALUES(nextval('category_id_sequence'), 'Dresses', '/category/pantolon.jpg', 0, null, 'system', '2025-04-12', NULL, NULL);
+VALUES(nextval('category_id_sequence'), 'Elektronik', '/category/pantolon.jpg', 0, null, 'system', '2025-04-12', NULL, NULL);
 
 INSERT INTO public.category
 (id, category_name, image_url, total_product_count, parent_id, created_by, created_date, updated_by, updated_date)
-VALUES(nextval('category_id_sequence'), 'Shirts', '/category/gomlek.jpg', 0, null, 'system', '2025-04-12', NULL, NULL);
+VALUES(nextval('category_id_sequence'), 'Moda', '/category/gomlek.jpg', 0, null, 'system', '2025-04-12', NULL, NULL);
 
 INSERT INTO public.category
 (id, category_name, image_url, total_product_count, parent_id, created_by, created_date, updated_by, updated_date)
-VALUES(nextval('category_id_sequence'), 'Jeans', '/category/tshirt.jpg', 0, null, 'system', '2025-04-12', NULL, NULL);
+VALUES(nextval('category_id_sequence'), 'Ev, Yaşam, Kırtasiye, Ofis', '/category/tshirt.jpg', 0, null, 'system', '2025-04-12', NULL, NULL);
 
 INSERT INTO public.category
 (id, category_name, image_url, total_product_count, parent_id, created_by, created_date, updated_by, updated_date)
-VALUES(nextval('category_id_sequence'), 'Swimwear', '/category/tshirt.jpg', 0, null, 'system', '2025-04-12', NULL, NULL);
+VALUES(nextval('category_id_sequence'), 'Oto, Bahçe, Yapı Market', '/category/tshirt.jpg', 0, null, 'system', '2025-04-12', NULL, NULL);
 
 INSERT INTO public.category
 (id, category_name, image_url, total_product_count, parent_id, created_by, created_date, updated_by, updated_date)
-VALUES(nextval('category_id_sequence'), 'Men''s Dresses', '/category/tshirt.jpg', 0, 100000, 'system', '2025-04-12', NULL, NULL);
+VALUES(nextval('category_id_sequence'), 'Anne, Bebek, Oyuncak', '/category/tshirt.jpg', 0, null, 'system', '2025-04-12', NULL, NULL);
 
 INSERT INTO public.category
 (id, category_name, image_url, total_product_count, parent_id, created_by, created_date, updated_by, updated_date)
-VALUES(nextval('category_id_sequence'), 'Women''s Dresses', '/category/tshirt.jpg', 0, 100000, 'system', '2025-04-12', NULL, NULL);
+VALUES(nextval('category_id_sequence'), 'Spor, Outdoor', '/category/tshirt.jpg', 0, null, 'system', '2025-04-12', NULL, NULL);
 
 INSERT INTO public.category
 (id, category_name, image_url, total_product_count, parent_id, created_by, created_date, updated_by, updated_date)
-VALUES(nextval('category_id_sequence'), 'Baby''s Dresses', '/category/tshirt.jpg', 0, 100000, 'system', '2025-04-12', NULL, NULL);
+VALUES(nextval('category_id_sequence'), 'Kozmetik, Kişisel Bakım', '/category/tshirt.jpg', 0, null, 'system', '2025-04-12', NULL, NULL);
+
+INSERT INTO public.category
+(id, category_name, image_url, total_product_count, parent_id, created_by, created_date, updated_by, updated_date)
+VALUES(nextval('category_id_sequence'), 'Süpermarket, Pet Shop', '/category/tshirt.jpg', 0, null, 'system', '2025-04-12', NULL, NULL);
+
+INSERT INTO public.category
+(id, category_name, image_url, total_product_count, parent_id, created_by, created_date, updated_by, updated_date)
+VALUES(nextval('category_id_sequence'), 'Kitap, Müzik, Film, Hobi', '/category/tshirt.jpg', 0, null, 'system', '2025-04-12', NULL, NULL);
+
+
+---------- sub categories ----------
+INSERT INTO public.category
+(id, category_name, image_url, total_product_count, parent_id, created_by, created_date, updated_by, updated_date)
+VALUES(nextval('category_id_sequence'), 'Bilgisayar/Tablet', '/category/tshirt.jpg', 0, 100000, 'system', '2025-04-12', NULL, NULL);
+
+INSERT INTO public.category
+(id, category_name, image_url, total_product_count, parent_id, created_by, created_date, updated_by, updated_date)
+VALUES(nextval('category_id_sequence'), 'Yazıcılar & Projeksiyon', '/category/tshirt.jpg', 0, 100000, 'system', '2025-04-12', NULL, NULL);
+
+INSERT INTO public.category
+(id, category_name, image_url, total_product_count, parent_id, created_by, created_date, updated_by, updated_date)
+VALUES(nextval('category_id_sequence'), 'Telefon & Telefon Aksesuarları', '/category/tshirt.jpg', 0, 100000, 'system', '2025-04-12', NULL, NULL);
+
+INSERT INTO public.category
+(id, category_name, image_url, total_product_count, parent_id, created_by, created_date, updated_by, updated_date)
+VALUES(nextval('category_id_sequence'), 'Elektrikli Ev Aletleri', '/category/tshirt.jpg', 0, 100000, 'system', '2025-04-12', NULL, NULL);
